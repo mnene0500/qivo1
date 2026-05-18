282bd25446d07299ae39f8d6965967c9e417823b
Mnene <mnene1223@gmail.com>
2026-05-18 13:15:29 +0000

    'use server';
    /**
     * @fileOverview Payment actions placeholder.
     */
    
    ex

---
+++ src/app/actions/payment-actions.ts
@@ -0,0 +1,9 @@
+'use server';
+
+/**
+ * @fileOverview Payment actions placeholder.
+ */
+
+export async function initiatePayment() {
+  return { success: false, error: 'Payment service is currently disabled.' };
+}
